e18993ede62c30c0a49813881212cb6676b71a37
gusdn3477 <gusdn3477@naver.com>
2022-08-06 22:35:41 +0900

    chore: GNB 컴포넌트 삭제

--- src/layouts/SidebarLayout/Header/HeaderLayout.tsx
+++ src/layouts/SidebarLayout/Header/HeaderLayout.tsx
@@ -17,7 +17,6 @@ import CloseTwoToneIcon from '@mui/icons-material/CloseTwoTone';
 
 import HeaderButtons from './Buttons';
 import HeaderUserbox from './Userbox/UserBox';
-import HeaderMenu from '../../../components/GNB';
 
 const HeaderWrapper = styled(Box)(
   ({ theme }) => `
@@ -67,9 +66,7 @@ function Header() {
         divider={<Divider orientation="vertical" flexItem />}
         alignItems="center"
         spacing={2}
-      >
-        <HeaderMenu />
-      </Stack>
+      />
       <Box display="flex" alignItems="center">
         <HeaderButtons />
         <HeaderUserbox />
